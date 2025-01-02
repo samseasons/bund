5db839355b5a98fe34c06f52caa75862fd4ef7d0
samseasons <185178474+samseasons@users.noreply.github.com>
2025-01-02 11:06:36 +0000

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -1,1 +1,10 @@
-// done
+// node done
+
+import fs from 'fs'
+import path from 'path'
+
+function func () {}
+function return_null () { return null }
+function return_this () { return this }
+function return_true () { return true }
+function return_false () { return false }
