60287b5e3e3a3f79bea40f61c6861ad980d4354a
samseasons <185178474+samseasons@users.noreply.github.com>
2025-01-12 11:00:07 +0000

    Add files via upload

--- bundle.js
+++ bundle.js
@@ -16,13 +16,64 @@ class tree {
 }
 tree.prototype.func = function (type, args, props) {
   this.type = type
-  if (props) args.forEach(arg => { this[arg] = props[arg] })
+  if (props) args.forEach(arg => this[arg] = props[arg])
+}
+tree.prototype.ascend = func
+tree.prototype.branch = func
+tree.prototype._copy = function (deep) {
+  if (deep) {
+    let self = this.copy()
+    return self.transform(new transforms(function (root) {
+      if (root != self) return root.copy(true)
+    }))
+  }
+  return new this.constructor(this)
+}
+tree.prototype.copy = function (deep) {
+  return this._copy(deep)
+}
+tree.prototype.equals = return_false
+tree.prototype.observe = function (observer) {
+  return observer.observe(this)
+}
+tree.prototype.transform = function (trees, trim) {
+  trees.push(this)
+  let transformed
+  if (trees.before) transformed = trees.before(this, this.ascend, trim)
+  if (transformed === undefined) {
+    transformed = this
+    this.ascend(transformed, trees)
+    if (trees.after) {
+      let after = trees.after(transformed, trim)
+      if (after) transformed = after
+    }
+  }
+  trees.pop()
+  return transformed
 }
 
 class ment extends tree {
   constructor (props) {
     super()
     this.func('ment', ['a', 'z', 'f'], props)
+  }
+}
+
+class observes {
+  constructor (callback) {
+    this.callback = callback
+    this.stack = []
+  }
+  observe (root, ascend) {
+    this.stack.push(root)
+  }
+}
+
+class transforms extends observes {
+  constructor (before, after) {
+    super()
+    this.after = after
+    this.before = before
   }
 }
 
@@ -40,13 +91,13 @@ function parse (text, opt) {
 
 function build (of, fo, opt={}) {
   out(opt.la)
-  opt.top = new tree({})
-  let ob = []
+  opt.top = new tree()
   let om = [of]
+  let oq = []
   let text
   while (om.length) {
     of = om[0]
-    if (ob.includes(of)) {
+    if (oq.includes(of)) {
       om.shift(1)
     } else {
       try {
@@ -56,8 +107,8 @@ function build (of, fo, opt={}) {
       }
       opt.of = of
       opt.om = om
-      opt.ob = ob
-      ob.push(of)
+      opt.oq = oq
+      oq.push(of)
       opt = parse(text, opt)
       om = opt.om
     }
